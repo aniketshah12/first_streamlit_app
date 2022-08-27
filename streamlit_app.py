e6663d76b51f9bf9860063fe0d4acfe73ade32ff
aniketshah12 <63900171+aniketshah12@users.noreply.github.com>
2022-08-27 15:33:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,9 @@ streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-boiled Free Egg Range')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+import pandas
+my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
 
 #New section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
