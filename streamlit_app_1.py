24a12debd9f862da61ff0c8f18e3eec109a0637c
aniketshah12 <63900171+aniketshah12@users.noreply.github.com>
2022-08-27 21:02:26 +0530

    Update streamlit_app_1.py

--- streamlit_app_1.py
+++ streamlit_app_1.py
@@ -32,7 +32,6 @@ try:
 if not fruit_choice:
    streamlit.error("Please select a fruit to get information.")
 else:  
-
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 #streamlit.text(fruityvice_response.json())
 
