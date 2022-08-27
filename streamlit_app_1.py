d5e90baef6ba7efc39a319ce869416a5d00dbba5
aniketshah12 <63900171+aniketshah12@users.noreply.github.com>
2022-08-27 16:03:43 +0530

    Create streamlit_app_1.py

---
+++ streamlit_app_1.py
@@ -0,0 +1,10 @@
+import streamlit
+import snowflake.connector
+import requests
+
+
+streamlit.title('My parents new healthy dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-boiled Free Egg Range')
